5631e162881f39a45cbc1429b46ea07161931dfe
mingaleev.d92@mail.ru <tatarstan1204BTc)>
2022-10-22 07:41:05 +0300

    create navigation

--- app/src/main/java/com/example/bslist/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/bslist/ui/activities/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.bslist.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.bslist.R
+import com.example.bslist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var binding: ActivityMainBinding
+   private lateinit var navController: NavController
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_main)
+      binding = ActivityMainBinding.inflate(layoutInflater)
+      setContentView(binding.root)
+
+      val navHostFragment =
+         supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+      navController = navHostFragment.navController
    }
 }
--- app/src/main/java/com/example/bslist/ui/fragments/SetTripBudgetFragment.kt
+++ app/src/main/java/com/example/bslist/ui/fragments/SetTripBudgetFragment.kt
@@ -5,30 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.bslist.databinding.FragmentShoppingListBinding
+import com.example.bslist.R
+import com.example.bslist.databinding.FragmentSetBudgetBinding
 
+/**
+ * @author : Mingaleev D
+ * @data : 22/10/2022
+ */
 
-class ShoppingListFragment : Fragment() {
+class SetTripBudgetFragment:Fragment(R.layout.fragment_set_budget) {
 
-   private var mBinding: FragmentShoppingListBinding? = null
+   private var mBinding:FragmentSetBudgetBinding? = null
    private val binding get() = mBinding!!
 
    override fun onCreateView(
-      inflater: LayoutInflater, container: ViewGroup?,
+      inflater: LayoutInflater,
+      container: ViewGroup?,
       savedInstanceState: Bundle?
    ): View {
-      mBinding = FragmentShoppingListBinding.inflate(inflater, container, false)
+      mBinding = FragmentSetBudgetBinding.inflate(inflater, container, false)
       return binding.root
    }
 
    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
       super.onViewCreated(view, savedInstanceState)
+
    }
 
    override fun onDestroy() {
       super.onDestroy()
       mBinding = null
    }
-
-
 }
